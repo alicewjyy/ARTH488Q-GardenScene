473f7fccc0f256785025fedd4e7ecd196a56beeb
Ahussein04 <ahussein0428@gmail.com>
2026-03-29 16:00:24 -0400

    i hate the second movie

---
+++ Assets/Scripts/mission_prompt.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class mission_prompt : MonoBehaviour
+{
+    public GameObject missionPanel;
+    public GameObject interactPrompt;
+
+    private bool playerInRange = false;
+
+
+
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        interactPrompt.SetActive(false);
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interactPrompt.SetActive(playerInRange);
+
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E pressed");
+            missionPanel.SetActive(!missionPanel.activeSelf);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
