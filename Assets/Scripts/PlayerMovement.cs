2e95a3d83bc9e13be6e12731f89911336044f421
AnnaMarchler <marchleranna@gmail.com>
2026-04-06 09:48:20 -0400

    Sprite Animation
    - Added assets folder with sprites and animations subfolders
    - Added art team illustrations to sprite folder
    - Updated player movement script and added basic idle/walk animation for Majnun

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+// Player movement and sprite flipping + animation code taken from https://youtu.be/pYu36PLmdq0?si=73RweqBqEnpRApcs and https://youtu.be/Sg_w8hIbp4Y?si=0snSYHyHd8lIdDUd - Anna M.
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,21 +7,29 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float movespeed = 5f;
+    float horizontalInput;
+    public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
     private float input;
+    bool isFacingRight = false;
+
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveInput = 0f;
+        horizontalInput = Input.GetAxis("Horizontal"); //A, D, and Left and Right Arrow keys
+
+        //Old Movement code
+        /*float moveInput = 0f;
 
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
         {
@@ -30,8 +40,27 @@ public class PlayerMovement : MonoBehaviour
             moveInput = 1f;
         }
 
-        rb.linearVelocity = new Vector2(moveInput * movespeed, rb.linearVelocity.y);
-        
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y); */
+
+        FlipSprite();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
